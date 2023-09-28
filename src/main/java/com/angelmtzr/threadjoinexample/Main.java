23a164f472808d4d28909fab9aeb1492bb89cefc
Ángel Martínez Rodríguez <angel.martinez.rodriguez-EXT@continental-corporation.com>
2023-09-28 10:50:38 -0600

    Implemented join so that Main waits for all threads to finish.

--- src/main/java/com/angelmtzr/threadjoinexample/Main.java
+++ src/main/java/com/angelmtzr/threadjoinexample/Main.java
@@ -1,19 +1,38 @@
 package com.angelmtzr.threadjoinexample;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         var data = DataCenter.getInstance(0);
+
+        LOGGER.log(Level.INFO, "Initial value: " + data.getValue());
+
+        ArrayList<MyThread> threads = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             var t = new MyThread(data, String.valueOf(i));
             t.start();
+            threads.add(t);
         }
+
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                LOGGER.log(Level.WARNING, "Thread " + t.getName() + " was interrupted.");
+            }
+        });
+
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Logger.getLogger(Main.class.getName()).info(String.valueOf(data.getValue()));
+
+        LOGGER.log(Level.INFO, "Final value: " + data.getValue());
     }
 }
